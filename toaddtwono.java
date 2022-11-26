2d5b7ab0f42b22be7d78c4cdd9de8b49f4dc5fdd
dhaniram24005 <87693749+dhaniram24005@users.noreply.github.com>
2022-11-26 22:16:05 +0530

    Add files via upload

---
+++ toaddtwono.java
@@ -0,0 +1,9 @@
+public class toaddtwono {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("The sum of the two no is : " + sum);
+    }
+
+}
